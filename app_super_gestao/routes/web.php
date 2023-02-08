b393f84de312e951abfda0f12155cb594fa4d570
Antonio Carlos Macedo <carloosjr.m@gmail.com>
2023-02-08 08:33:40 -0300

    Subindo projeto app_super_gestao

---
+++ app_super_gestao/routes/web.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PrincipalController;
+use App\Http\Controllers\SobreNosController;
+use App\Http\Controllers\ContatoController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/',[PrincipalController::class,'principal'])->name('site.index');
+Route::get('/sobre-nos',[SobreNosController::class,'sobreNos'])->name('site.sobrenos');
+Route::get('/contato',[ContatoController::class,'contato'])->name('site.contato');
+
+
+Route::get('/login', function(){return 'Login';})->name('site.login');
+Route::prefix('/app')->group(function(){
+    Route::get('/clientes', function(){return 'Clientes';})->name('app.clientes');
+    Route::get('/fornecedores', [FornecedorController::class,'index'])->name('app.fornecedores');
+    Route::get('/produtos', function(){return 'Produtos';})->name('app.produtos');
+});
+
+Route::fallback(function(){
+    echo 'A rota acessada não existe. <a href="'.route('site.index').'">clique aqui</a> para a pagina inicial';
+});
